25e46f94691968b960ebb3dc923b57dfc71ba6fc
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2019-01-21 20:45:58 -0800

    configures webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require ('path');
+var DIST_DIR = path.join (__dirname, '/public');
+var SRC_DIR = path.join (__dirname, '/src');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react'],
+        },
+      },
+    ],
+  },
+};
